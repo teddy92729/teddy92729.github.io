52f27dfdffd475b5fb4a51da0e952698baedd476
teddy92729 <teddy19026@yahoo.com.tw>
2022-02-04 21:06:25 +0800

    Create shader.js

---
+++ ani-ss/dist/shader.js
@@ -0,0 +1,12 @@
+(async()=>{
+  var anime4k=await fetch("https://teddy92729.github.io/ani-ss/dist/Anime4K_Upscale_Denoise_CNN_x2_VL.glsl").then(r=>r.text());
+  // console.log(anime4k);
+  window.getAnime4k=function(){
+    return anime4k;
+  }
+
+  addCustomProgram(getAnime4k());
+  
+  console.log("addShader");
+// vid.src=vid.src;   
+})();  
