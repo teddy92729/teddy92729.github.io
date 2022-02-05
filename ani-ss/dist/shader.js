e9bd6a73af165e62a1e1e5528a634408c8d834de
teddy92729 <teddy19026@yahoo.com.tw>
2022-02-06 02:11:06 +0800

    Update shader.js

--- ani-ss/dist/shader.js
+++ ani-ss/dist/shader.js
@@ -4,8 +4,8 @@
   console.log("Adding shader");
   var anime4k=await fetch("https://teddy92729.github.io/ani-ss/dist/Anime4K_Upscale_Deblur_DoG_x2.glsl").then(r=>r.text());
   addCustomProgram(anime4k);
-  anime4k=await fetch("https://teddy92729.github.io/ani-ss/dist/Anime4K_Thin_HQ.glsl").then(r=>r.text());
-  addCustomProgram(anime4k);
+//   anime4k=await fetch("https://teddy92729.github.io/ani-ss/dist/Anime4K_Thin_HQ.glsl").then(r=>r.text());
+//   addCustomProgram(anime4k);
   console.log("shader added");
   if(vid){
     vid.dispatchEvent(new Event("loadeddata"));
