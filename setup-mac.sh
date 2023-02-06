1faf73e0780a318bf4534cec3c82f6f7becee9e8
teddy92729 <teddy19026@yahoo.com.tw>
2023-02-06 13:13:09 +0800

    Add files via upload

--- setup-mac.sh
+++ setup-mac.sh
@@ -4,20 +4,13 @@
 # sudo ip link set dev eth0 up
 # sleep 5s
 
-check_network() {
-  ping -q -c3 8.8.8.8 > /dev/null
-  if [ $? -ne 0 ]; then
-  else
-    sudo ip link set dev eth0 down
-    sudo ip link set dev eth0 address 50:eb:f6:54:ed:13
-    sudo ip link set dev eth0 up
-    sleep 10s
-  fi
+setup_network() {
+    ping -q -c3 8.8.8.8 > /dev/null
+    if [ $? -ne 0 ]; then
+        sudo ip link set dev eth0 down
+        sudo ip link set dev eth0 address 50:eb:f6:54:ed:13
+        sudo ip link set dev eth0 up
+        sleep 10s
+    fi
 }
-
-# Call the function
-while [1 -eq 1]
-do
-    check_network
-    sleep 1m
-done
+setup_network
