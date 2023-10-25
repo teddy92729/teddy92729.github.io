41be7a58f695e79e1af329c88a3f4a5bc7e12020
HUANG, YUN-TAI <teddy92729@gmail.com>
2023-10-25 11:09:55 +0800

    Update elementCreated.js

--- elementCreated.js
+++ elementCreated.js
@@ -1,17 +1,31 @@
-function elementCreated(selector){
-  return new Promise((r)=>{
-    let target=document.querySelector(selector);
-    if(target)
-      r(target);
-    else{
-      const observer = new MutationObserver(()=>{
-        target = document.querySelector(selector);
-        if (target){
-          observer.disconnect();
-          r(target);
+function elementCreated(selector) {
+    return new Promise((r) => {
+        //first check if the element already exists
+        let target = document.querySelector(selector);
+        if (target)
+            r(target);
+        else {
+            try {
+                //use a mutation observer to wait for it to be added to the DOM
+                const observer = new MutationObserver(() => {
+                    target = document.querySelector(selector);
+                    if (target) {
+                        observer.disconnect();
+                        r(target);
+                    }
+                });
+                observer.observe(document.body, { childList: true, subtree: true });
+            }
+            catch (e) {
+                //fallback to polling
+                let id = setInterval(() => {
+                    target = document.querySelector(selector);
+                    if (target) {
+                        clearInterval(id);
+                        r(target);
+                    }
+                }, 100);
+            }
         }
-      });
-      observer.observe(document.body,{childList: true, subtree: true});
-    }
-  });
+    });
 }
